bf76592af76e7fdc52e9db3650cb92980687fab7
praveenrajb005 <praveenrajb750@gmail.com>
2025-08-18 15:40:30 +0530

    Time: 55 ms (21.23%), Space: 49 MB (20.89%) - LeetHub

---
+++ 1967-longest-substring-of-all-vowels-in-order/1967-longest-substring-of-all-vowels-in-order.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestBeautifulSubstring(String word) {
+        char[] ch=word.toCharArray();
+        HashSet<Character> set=new HashSet<>();
+        int max=0;
+        set.add(ch[0]);
+        int ind=0;
+        for(int i=1;i<ch.length;i++){
+            if(ch[i]>=ch[i-1]){
+                set.add(ch[i]);
+            }
+            else{
+                if(set.size()==5){
+                    max=Math.max(max,i-ind);
+                    ind=i;
+                    set.clear();
+                }
+                else{
+                    ind=i;
+                    set.clear();
+                    set.add(ch[i]);
+                }
+            }
+        }
+        if(set.size()==5)
+        max=Math.max(max,ch.length-ind);
+        return max;
+    }
+}
